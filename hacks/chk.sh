dc3b74a9df3c951cdad22986ab3539a0f5c9b9df
Henry Gabryjelski <henrygab@users.noreply.github.com>
2025-03-27 22:42:02 -0700

    Quit the script when any step fails

--- hacks/chk.sh
+++ hacks/chk.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# set the script to exit on any command that returns a non-zero status
+set -e
+
 # First version:
 # * Current working directory must be depot root.
 # * Run as `./hacks/chk.sh`
@@ -11,18 +14,19 @@
 # * [ ] Maybe set bash to exit on errors ... `set -e`?
 
 # update CMake for both rp2040 builds and rp2350 builds
-cmake -S . -B build_rp2040 -DPICO_SDK_FETCH_FROM_GIT=TRUE
-cmake -S . -B build_rp2350 -DPICO_SDK_FETCH_FROM_GIT=TRUE -DBP_PICO_PLATFORM=rp2350
+cmake -S . -B build_rp2040 -DPICO_SDK_FETCH_FROM_GIT=TRUE                            || exit 11
+cmake -S . -B build_rp2350 -DPICO_SDK_FETCH_FROM_GIT=TRUE -DBP_PICO_PLATFORM=rp2350  || exit 12
 
 # optionally, do a clean build each time
-cmake --build ./build_rp2040 --parallel --target clean
-cmake --build ./build_rp2350 --parallel --target clean
+# How to make build fail on all warnings?
+cmake --build ./build_rp2040 --parallel --target clean || exit 31
+cmake --build ./build_rp2350 --parallel --target clean || exit 32
 
 # optionally, update the translation files
 # TODO: Run `./src/translation/json2h.py` to generate updated translation files
-python ./src/translation/json2h.py
+python ./src/translation/json2h.py || exit 51
 
 # build everything
-cmake --build ./build_rp2040 --parallel --target all
-cmake --build ./build_rp2350 --parallel --target all
+cmake --build ./build_rp2040 --parallel --target all || exit 71
+cmake --build ./build_rp2350 --parallel --target all || exit 72
 
