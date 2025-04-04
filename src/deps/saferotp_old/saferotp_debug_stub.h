2bdbf33b14596b4d16b9e867b753a075cafb7f1e
Henry Gabryjelski <henrygab@users.noreply.github.com>
2025-04-04 13:27:09 -0700

    Removing prior version of library

--- src/deps/saferotp_old/saferotp_debug_stub.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// macros used elsewhere
-#define PRINT_FATAL(...)
-#define PRINT_ERROR(...)
-#define PRINT_WARNING(...)
-#define PRINT_INFO(...)
-#define PRINT_VERBOSE(...)
-#define PRINT_DEBUG(...)
