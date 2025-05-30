051ed32ebfa97e5a78b4d2bb30714e0a02f5719a
Henry Gabryjelski <henrygab@users.noreply.github.com>
2025-05-30 00:29:28 -0700

    Dump comment via RTT

--- src/commands/global/cmd_comment.c
+++ src/commands/global/cmd_comment.c
@@ -9,8 +9,31 @@
 
 
 void cmd_comment_handler(uint32_t start_offset, uint32_t end_offset) {
+    start_offset = cmdln_pu(start_offset + cmdln.read_offset);
+    end_offset = cmdln_pu(end_offset + cmdln.read_offset);
 
-    PRINT_DEBUG("cmd_comment_handler() - [%d .. %d]\r\n", start_offset, end_offset);
+    // Total hack:
+    if (start_offset == end_offset) {
+        PRINT_DEBUG("##\r\n");
+    } else if (start_offset < end_offset) {
+        char last_char = cmdln.buf[end_offset];
+        cmdln.buf[end_offset] = 0x00; // null-terminate the string
+        PRINT_DEBUG("#%s%c\r\n", &cmdln.buf[start_offset], last_char);
+        cmdln.buf[end_offset] = last_char; // restore the last character
+    } else {
+        char end_of_buffer_char = cmdln.buf[UI_CMDBUFFSIZE - 1];
+        char last_char = cmdln.buf[end_offset];
+        cmdln.buf[UI_CMDBUFFSIZE - 1] = 0x00; // null-terminate the first half of the string
+        cmdln.buf[end_offset] = 0x00; // null-terminate the second half of the string
+        PRINT_DEBUG(
+            "#%s%c%s%c\r\n",
+            &cmdln.buf[start_offset], end_of_buffer_char,
+            &cmdln.buf[0], last_char
+            );
+        cmdln.buf[UI_CMDBUFFSIZE - 1] = end_of_buffer_char; // restore the end of buffer character
+        cmdln.buf[end_offset] = last_char; // restore the last character
+    }
+
     // TODO: log the current command line (the comment) into the debug output stream
     //       This will make it easier to correlate other debug output with
     //       where in a long script (or macro, or other set of commands) that output
@@ -25,8 +48,5 @@ void cmd_comment_handler(uint32_t start_offset, uint32_t end_offset) {
     //  copy the last character of the command line buffer to a temporary variable
     //  set that last character to 0x00
     //  treat g_...[start_offset] as a null-terminated string
-
-    // PRINT_FATAL("CmdlineComment: %s", comment_command_line.buf);
-    // PRINT_INFO("CmdlineComment: %s", comment_command_line.buf);
     return;
 }
